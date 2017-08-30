68c48536aeb3ee25ec7ca047f996f0d9b34b6d65
Jessica <jdhpabst@gmail.com>
2017-08-30 14:14:23 -0600

    updated button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
       <div className="home">
       <img src="http://www.xiriux.net/wp-content/uploads/android.png" id='phone_img'/>
       <h1>Critter Tracker</h1>
-      <p>LOGIN</p>
+      <div id="login-bttn">LOGIN</div>
       </div>
     );
   }
