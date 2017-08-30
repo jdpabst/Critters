377069b8825e21f6de5b3f20234af455c743455b
Jessica <jdhpabst@gmail.com>
2017-08-30 14:32:03 -0600

    updating phone background

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,6 +10,7 @@ class Home extends Component {
       <div className="home">
       <img src="http://www.xiriux.net/wp-content/uploads/android.png" id='phone_img'/>
       <div id="phone_content">
+        <div id="transparent_overlay"></div>
         <h1>Critter Tracker</h1>
         <div id="login-bttn">LOGIN</div>
       </div>
