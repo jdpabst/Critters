7057d7d7baa96424bc422a0ea26c2f95b7641da6
Jessica <jdhpabst@gmail.com>
2017-08-30 20:54:58 -0600

    added landing page folder and files

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Link from 'react-router-dom';
+
+import './Landing.css';
+
+
+class Landing extends Component {
+
+  render() {
+    return (
+      <div className="landing">
+        Landing page
+      </div>
+    );
+  }
+}
+
+
+export default Landing;
