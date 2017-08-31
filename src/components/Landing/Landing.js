f50c056e3fbbb244ef186fc33dd2d58317ec519c
Jessica <jdhpabst@gmail.com>
2017-08-31 11:40:47 -0600

    added x to drop-down menu

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -16,6 +16,13 @@ class Landing extends Component {
                   <div className="ham"></div>
                   <div className='tracks'></div>
                 </header>
+                <div className="drop_down">
+                  <div id="exit_menu"></div>
+                  <ul>
+                    <li>PROFILE</li>
+                    <li>SETTINGS</li>
+                  </ul>
+                </div>
                 <div id="profile"></div>
                 <div id="new_track">ADD CRITTER</div>
             </div>
