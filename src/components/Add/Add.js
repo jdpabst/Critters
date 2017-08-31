2c6d3bb1d5acdfaa5adca7cf7ff3a1dbceb7823a
Jessica <jdhpabst@gmail.com>
2017-08-31 13:55:35 -0600

    updating add pages

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -8,6 +8,22 @@ class Add extends Component {
   render() {
     return (
       <div className="add">
+        <div id="add_content_holder">
+        <img src="http://www.xiriux.net/wp-content/uploads/android.png" id='phone_img'/>
+        <div id="add_content">
+            <header className="header">
+              <div className="ham" onClick={ this.handleMenu }></div>
+            </header>
+            <div id='menu' className="drop_down">
+              <div id="exit_menu" onClick={ this.handleMenu }></div>
+                <ul>
+                    <li>PROFILE</li>
+                    <li>SETTINGS</li>
+                    <li>ABOUT</li>
+                </ul>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
