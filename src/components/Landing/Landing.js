a279f3a2e050f7a25ef7719dfb2dbb6e79a243ad
Jessica <jdhpabst@gmail.com>
2017-08-31 11:07:48 -0600

    working on header section of landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -11,9 +11,10 @@ class Landing extends Component {
       <div className="landing">
           <div id="landing_content_holder">
             <img src="http://www.xiriux.net/wp-content/uploads/android.png" id='phone_img'/>
+            <header className="header">
+              <div id="ham"></div>
+            </header>
             <div id="landing_content">
-                {/* <div id="landing_overlay"></div> */}
-                <div id='logo'></div>
                 <div id="profile"></div>
                 <div id="new_track">ADD NEW TRACK</div>
             </div>
