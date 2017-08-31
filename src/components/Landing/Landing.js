d9dd0dc529ea5928f7a7282d6e4ec1b7583dd544
Jessica <jdhpabst@gmail.com>
2017-08-31 11:04:59 -0600

    updating styling and re-working landing page layout

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -12,7 +12,7 @@ class Landing extends Component {
           <div id="landing_content_holder">
             <img src="http://www.xiriux.net/wp-content/uploads/android.png" id='phone_img'/>
             <div id="landing_content">
-                <div id="landing_overlay"></div>
+                {/* <div id="landing_overlay"></div> */}
                 <div id='logo'></div>
                 <div id="profile"></div>
                 <div id="new_track">ADD NEW TRACK</div>
