847d8f06e4983d8db6eafd35e33c89477a0aee5f
Jessica <jdhpabst@gmail.com>
2017-08-30 21:02:32 -0600

    trying to get phone image to show up on landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,6 +9,7 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
+        <img src="http://www.xiriux.net/wp-content/uploads/android.png" id='phone_img'/>
         <div id="new_track">ADD NEW TRACK</div>
       </div>
     );
